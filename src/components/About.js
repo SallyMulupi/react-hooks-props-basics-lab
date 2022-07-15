4f36ada1d57fe2dbac698bcf67e5f1f647a661fb
SallyMulupi <mulupisally2gmail.com>
2022-07-15 17:24:41 +0300

    insert image

--- src/components/About.js
+++ src/components/About.js
@@ -5,4 +5,5 @@ function About() {
     <div id="about"></div>
     <h2>About Me</h2>
     {prop.bio === "" && !prop.bio ? <p>don't display</p> : <p>{prop.bio}</p>}
+    <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
 export default About;
