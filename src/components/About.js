4388c67aa5ea5fd1a82cd27efdf6ca99133108e9
SallyMulupi <mulupisally2gmail.com>
2022-07-15 17:22:40 +0300

    add id

--- src/components/About.js
+++ src/components/About.js
@@ -2,15 +2,5 @@ import React from "react";
 
 function About() {
   return (
-    <div id="about">
-      <h2>About Me</h2>
-        {prop.bio === "" && !prop.bio ? <p>don't display</p> : <p>{prop.bio}</p>}
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <Links  github="https://github.com/liza"  linkedin ="https://www.linkedin.com/in/liza/"/>
-      {/* add your <Links /> component here */}
-    </div>
-  );
-}
-
-
+    <div id="about"></div>
 export default About;
