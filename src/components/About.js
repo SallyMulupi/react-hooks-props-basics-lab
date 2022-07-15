9aa01049c18c2f91f59318d1384f461227f64abc
SallyMulupi <mulupisally2gmail.com>
2022-07-15 17:18:34 +0300

    add file

--- src/components/About.js
+++ src/components/About.js
@@ -4,11 +4,13 @@ function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+        {prop.bio === "" && !prop.bio ? <p>don't display</p> : <p>{prop.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links  github="https://github.com/liza"  linkedin ="https://www.linkedin.com/in/liza/"/>
       {/* add your <Links /> component here */}
     </div>
   );
 }
+
 
 export default About;
---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Links = (prop) => {
+    return ( 
+        
+        <h3>
+        <a href="#github">{prop.github}</a>
+        <a href="#linkedin">{prop.linkedin}</a>
+        </h3>
+    )
+}
+
+export default Links
