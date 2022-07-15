6935d5febc57e1dabc911fb43d7052c7fa3f95fe
SallyMulupi <mulupisally2gmail.com>
2022-07-15 17:23:54 +0300

    declared

--- src/components/About.js
+++ src/components/About.js
@@ -4,4 +4,5 @@ function About() {
   return (
     <div id="about"></div>
     <h2>About Me</h2>
+    {prop.bio === "" && !prop.bio ? <p>don't display</p> : <p>{prop.bio}</p>}
 export default About;
