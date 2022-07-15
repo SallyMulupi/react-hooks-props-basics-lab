4f203160108de0fef6fc9694d5e9f68962514c05
SallyMulupi <mulupisally2gmail.com>
2022-07-15 17:25:05 +0300

    insert link

--- src/components/About.js
+++ src/components/About.js
@@ -6,4 +6,5 @@ function About() {
     <h2>About Me</h2>
     {prop.bio === "" && !prop.bio ? <p>don't display</p> : <p>{prop.bio}</p>}
     <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+    <Links  github="https://github.com/liza"  linkedin ="https://www.linkedin.com/in/liza/"/>
 export default About;
